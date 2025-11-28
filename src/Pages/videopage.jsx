38a66fae54b5c06b5e766fa7b7cf43589a281e62
Prajwalachar07 <prajwalachar413@gmail.com>
2025-11-28 11:40:14 +0530

    updated

--- src/Pages/videopage.jsx
+++ src/Pages/videopage.jsx
@@ -1,20 +1,20 @@
-"use client";
-import VideoSlider from "../Components/VideoSlider/VideoSlider";
+// "use client";
+// import VideoSlider from "../Components/VideoSlider/VideoSlider";
 
-const videos = [
-  { id: 1, src: "/videos/video2.mp4", poster: "/videos/poster1.png", title: "Intro", subtitle: "Short one-line description" },
-  { id: 2, src: "/videos/video1.mp4", poster: "/videos/poster1.jpg", title: "Intro", subtitle: "Short one-line description" },
-  { id: 3, src: "/videos/video3.mp4", poster: "/videos/poster3.jpg", title: "Intro", subtitle: "Short one-line description" },
-  { id: 4, src: "/videos/video4.mp4", poster: "/videos/poster4.png", title: "Intro", subtitle: "Short one-line description" },
-  { id: 5, src: "/videos/video1.mp4", poster: "/videos/poster1.png", title: "Intro", subtitle: "Short one-line description" },
-  { id: 6, src: "/videos/video1.mp4", poster: "/videos/poster1.png", title: "Intro", subtitle: "Short one-line description" },
-  { id: 7, src: "/videos/video1.mp4", poster: "/videos/poster1.png", title: "Intro", subtitle: "Short one-line description" },
-];
+// const videos = [
+//   { id: 1, src: "/videos/video2.mp4", poster: "/videos/poster1.png", title: "Intro", subtitle: "Short one-line description" },
+//   { id: 2, src: "/videos/video1.mp4", poster: "/videos/poster1.jpg", title: "Intro", subtitle: "Short one-line description" },
+//   { id: 3, src: "/videos/video3.mp4", poster: "/videos/poster3.jpg", title: "Intro", subtitle: "Short one-line description" },
+//   { id: 4, src: "/videos/video4.mp4", poster: "/videos/poster4.png", title: "Intro", subtitle: "Short one-line description" },
+//   { id: 5, src: "/videos/video1.mp4", poster: "/videos/poster1.png", title: "Intro", subtitle: "Short one-line description" },
+//   { id: 6, src: "/videos/video1.mp4", poster: "/videos/poster1.png", title: "Intro", subtitle: "Short one-line description" },
+//   { id: 7, src: "/videos/video1.mp4", poster: "/videos/poster1.png", title: "Intro", subtitle: "Short one-line description" },
+// ];
 
-export default function VideoSection() {
-  return (
-    <main className="p-8">
-      <VideoSlider videos={videos} visibleCount={3} />
-    </main>
-  );
-}
+// export default function VideoSection() {
+//   return (
+//     <main className="p-8">
+//       <VideoSlider videos={videos} visibleCount={3} />
+//     </main>
+//   );
+// }
